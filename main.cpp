81e259f30e7b2f97f76ec7afc6109f0f7ec91d94
HSUABI <seongwooksong2107@gmail.com>
2018-07-21 13:11:52 +0900

    Print ip_src , ip_dst

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "protocol_structure.h"
 #include "printarr.h"
 #include "protocol_check.h"
+#define ETHER_LEN 14
 
 void usage() {
   printf("syntax: pcap_test <interface>\n");
@@ -18,6 +19,7 @@ int main(int argc, char* argv[]) {
   struct sniff_ethernet *ethernet;
   struct sniff_ip *ip;
   struct sniff_tcp *tcp;
+  u_int size_ip;
 
   char* dev = argv[1];
   char errbuf[PCAP_ERRBUF_SIZE];
@@ -32,14 +34,25 @@ int main(int argc, char* argv[]) {
     const u_char* packet;
     int res = pcap_next_ex(handle, &header, &packet);
     ethernet = (struct sniff_ethernet*)packet;
+    ip = (struct sniff_ip*)(packet+ETHER_LEN);
+    size_ip = IP_HL(ip)*4; 
+    
     if (res == 0) continue;
     if (res == -1 || res == -2) break;
     printf("Destination MacAddress\t:");
     printarr(ethernet->ether_dhost,ETHER_ADDR_LEN);
     printf("Source MacAddress\t:");
     printarr(ethernet->ether_shost,ETHER_ADDR_LEN);
-    printf("Ether Type\t:%hu\n",ethernet->ether_type);
-    if(ip_check(ethernet->ether_type))  printf("It is IP TYPE\n");
+    
+    if(ip_check(ethernet->ether_type))
+    {
+
+      printf("source ip\t:%x\n",ip->ip_src);
+      printf("source ip\t:%x\n",ip->ip_dst);
+
+      //if(tcp_check(ip->ip_p)) printf("it is tcp\n");
+    }
+    
 
   }
 
